39550b4b4fd7eba5c4d561686a28f8009e16277b
santu essence <santu.essence@gmail.com>
2023-08-14 19:51:41 +0530

    Command changes

--- lib/generators/lead_controller_generator.rb
+++ lib/generators/lead_controller_generator.rb
@@ -1,4 +1,4 @@
-class LeadControllerGenerator < Rails::Generators::NamedBase
+class Generators::LeadControllerGenerator < Rails::Generators::NamedBase
   def create_controller_files
     create_file "#{Rails.root}/controllers/#{file_name}_controller.rb", <<~RUBY
       class #{file_name.capitalize}Controller < ApplicationController
--- lib/generators/lead_model_generator.rb
+++ lib/generators/lead_model_generator.rb
@@ -1,4 +1,4 @@
-class LeadModelGenerator < Rails::Generators::NamedBase
+class Generators::LeadModelGenerator < Rails::Generators::NamedBase
   def create_model
     create_file "#{Rails.root}/app/models/#{file_name}.rb", <<~RUBY
       class #{file_name.capitalize} < ActiveRecord::Base
