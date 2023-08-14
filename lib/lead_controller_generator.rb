5e9a8eff8d15dba24d7fe7110569cf575d0c4e5d
santu essence <santu.essence@gmail.com>
2023-08-14 19:48:11 +0530

    Command changes

---
+++ lib/lead_controller_generator.rb
@@ -0,0 +1,9 @@
+class LeadControllerGenerator < Rails::Generators::NamedBase
+  def create_controller_files
+    create_file "#{Rails.root}/controllers/#{file_name}_controller.rb", <<~RUBY
+      class #{file_name.capitalize}Controller < ApplicationController
+        
+      end
+    RUBY
+  end
+end
