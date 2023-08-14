2546cf681861a407726f18f66362d903dd073fe1
santu essence <santu.essence@gmail.com>
2023-08-14 19:38:59 +0530

    Some changes

--- lib/generators/lead_model_generator.rb
+++ lib/generators/lead_model_generator.rb
@@ -1,8 +1,9 @@
-module Generator
-  class ModelGenerator < Rails::Generators::NamedBase
+module Generators
+  class LeadModelGenerator < Rails::Generators::NamedBase
     def create_model
       create_file "#{Rails.root}/app/models/#{file_name}.rb", <<~RUBY
         class #{file_name.capitalize} < ActiveRecord::Base
+          puts "i am lead #{file_name} model"
         end
       RUBY
     end
