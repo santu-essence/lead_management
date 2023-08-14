c0d1217b647431e47946186e0083a67aea6f01c3
santu essence <santu.essence@gmail.com>
2023-08-14 19:43:08 +0530

    Minor changes

--- lib/generators/lead_controller_generator.rb
+++ lib/generators/lead_controller_generator.rb
@@ -3,7 +3,7 @@ module Generators
     def create_controller_files
       create_file "#{Rails.root}/controllers/#{file_name}_controller.rb", <<~RUBY
         class #{file_name.capitalize}Controller < ApplicationController
-          puts "i am lead #{file_name} controller"
+          
         end
       RUBY
     end
--- lib/generators/lead_model_generator.rb
+++ lib/generators/lead_model_generator.rb
@@ -3,7 +3,7 @@ module Generators
     def create_model
       create_file "#{Rails.root}/app/models/#{file_name}.rb", <<~RUBY
         class #{file_name.capitalize} < ActiveRecord::Base
-          puts "i am lead #{file_name} model"
+          
         end
       RUBY
     end
