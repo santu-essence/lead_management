36d33bb148cd88274baabc151806d9d6e462469f
santu essence <santu.essence@gmail.com>
2023-08-16 10:43:10 +0530

    Minor changes

---
+++ lib/generators/lead_management/lead_model_generator.rb
@@ -0,0 +1,13 @@
+module LeadManagement
+  module Generators
+    class LeadModelGenerator < Rails::Generators::NamedBase
+      def create_model
+        create_file "#{Rails.root}/app/models/#{file_name}.rb", <<~RUBY
+          class #{file_name.capitalize} < ActiveRecord::Base
+            
+          end
+        RUBY
+      end
+    end
+  end
+end
